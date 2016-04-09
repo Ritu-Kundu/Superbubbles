4c2bbf60a2050e9c657c96f3fda183798c34ccc3
E. A. Davison <axedude1@gmail.com>
2016-04-09 18:17:46 +0100

    Corrected .gitignore and added the moved Logger back

---
+++ include/eadib_log/log_outputs/LogOutput.h
@@ -0,0 +1,21 @@
+//
+// Created by alwyn on 3/15/16.
+//
+
+#ifndef EADLIB_LOGOUTPUT_H
+#define EADLIB_LOGOUTPUT_H
+
+#include <iostream>
+#include "eadib_log/log_formatters/Formatter.h"
+
+namespace eadlib {
+    namespace log {
+        class LogOutput {
+          public:
+            virtual void open_ostream( const std::string &name ) = 0;
+            virtual void close_ostream() = 0;
+            virtual void write( const std::string &msg ) = 0;
+        };
+    }
+}
+#endif //EADLIB_LOGOUTPUT_H
